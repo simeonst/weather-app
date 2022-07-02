69de1787caf1cd4cdc95d8b2ebe5ef8fa33789a1
simeonst <sstakicisb@gmail.com>
2022-07-02 18:59:20 +0200

    Implement basic layout, colors, margins, borders

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,6 +3,16 @@ import "./Forecast.scss";
 
 export default class Forecast extends Component {
   render() {
-    return <div className="forecast">Forecast</div>;
+    return (
+      <div className="forecast">
+        <div className="current">Today</div>
+        <div className="next-days">
+          <div className="day">Day +1</div>
+          <div className="day">Day +2</div>
+          <div className="day">Day +3</div>
+          <div className="day">Day +4</div>
+        </div>
+      </div>
+    );
   }
 }
