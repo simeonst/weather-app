a10a88d0596210404be7c04acb23b2880c163cf5
simeonst <sstakicisb@gmail.com>
2022-07-02 18:11:29 +0200

    Implement basic layout, project folder structure, Sass, CSS variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import WeatherWrapper from "./components/WeatherWrapper/WeatherWrapper";
+
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <WeatherWrapper />
     </div>
   );
 }
---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Forecast.scss";
+
+export default class Forecast extends Component {
+  render() {
+    return <div className="forecast">Forecast</div>;
+  }
+}
