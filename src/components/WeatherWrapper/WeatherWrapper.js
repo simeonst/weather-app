5aea0c0d2396ef2b393c3ba37d70b4d04cd094ce
simeonst <sstakicisb@gmail.com>
2022-07-03 20:24:17 +0200

    Data fetching setup on componentDidMount

--- src/components/WeatherWrapper/WeatherWrapper.js
+++ src/components/WeatherWrapper/WeatherWrapper.js
@@ -3,6 +3,42 @@ import Forecast from "../Forecast/Forecast";
 import "./WeatherWrapper.scss";
 
 export default class WeatherWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCity: {
+        lat: 49.28,
+        lon: -123.12,
+      },
+      data: [],
+      loading: true,
+      error: false,
+    };
+  }
+
+  async componentDidMount() {
+    const { lat, lon } = this.state.selectedCity;
+    const exclude = "minutely,hourly,alerts";
+
+    try {
+      const url = `${process.env.REACT_APP_API_URL}/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&units=metric&appid=${process.env.REACT_APP_API_KEY}`;
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+
+      const json = await response.json();
+      this.setState({ loading: false, data: json });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        error: "Woops, something went wrong, please try again later",
+        loading: false,
+      });
+    }
+  }
+
   render() {
     return (
       <div className="weather-wrapper">
